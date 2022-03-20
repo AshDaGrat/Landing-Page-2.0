ba91de0f2cc140028eaac486088d19d48f54b3df
Ashwin V <ashwin.v336@gmail.com>
2022-03-20 22:17:31 +0530

    .

---
+++ src/sidebar.js
@@ -0,0 +1,13 @@
+const sidebar = () => {
+    return(
+        <div className="fixed top-0 left-0 w=12 h-screen">
+            <i>A</i>
+            <i>About</i>
+            <i>Skills</i>
+            <i>Acheivements</i>
+            <i>Contact</i>
+        </div>
+    );
+};
+
+export default sidebar;
